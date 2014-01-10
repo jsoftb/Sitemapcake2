d8cff8efbf0caa25e4e06ee4bae40e6d85db5275
Juan Bautista Gimenez Sendiu <neojoda@yahoo.es>
2014-01-09 19:55:42 -0700

    Defined basic Component structure

--- Controller/Component/SitemapComponent.php
+++ Controller/Component/SitemapComponent.php
@@ -4,6 +4,31 @@ App::uses('Component', 'Controller');
 
 class SitemapComponent extends Component {
 	
+	/**
+	 * 
+	 * @param string $controllername
+	 * @param array $excludeActions
+	 */
+	public function addController($controllername, $excludeActions = array()) {
+		
+	}
 	
+	/**
+	 * 
+	 * If no condition is given, it get all the records of a model
+	 * $allArticles = $modelName->find('all');
+	 * It must exists the Models' controller
+	 * 
+	 * @param Model $modelName
+	 * @param array $conditions
+	 */
+	public function addModel ($modelName, $conditions = array()) {
+		
+	}
+	
+	
+	public function generateSitemap() {
+		
+	}
 	
 }
