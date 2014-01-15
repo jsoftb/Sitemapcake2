ccc0d14ff21fa62401b87efa78f0ba687e4c5c64
Juan Bautista Gimenez Sendiu <neojoda@yahoo.es>
2014-01-14 19:58:52 -0700

    License

--- Config/routes.php
+++ Config/routes.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * SitemapComponent Controller
+ *
+ * Pretty much just baked admin actions except add/edit use generateTreeList()
+ * for finding the parents so you see the hierarchy.
+ *
+ * @author Juan Gimenez <neojoda@gmail.com>
+ * @link http://www.montcat.org/portfolio
+ * @copyright (c) 2014 Juan Gimenez
+ * @license MIT License - http://www.opensource.org/licenses/mit-license.php *
+ */
 
 Router::connect('/sitemap.xml', array('plugin'=>'Sitemapcake2', 'controller' => 'sitemap', 'action' => 'index'));
  
--- Controller/SitemapController.php
+++ Controller/SitemapController.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * BlogPostCategories Controller
+ * SitemapComponent Controller
  *
  * Pretty much just baked admin actions except add/edit use generateTreeList()
  * for finding the parents so you see the hierarchy.
  *
- * @author Neil Crookes <neil@crook.es>
- * @link http://www.neilcrookes.com http://neil.crook.es
- * @copyright (c) 2011 Neil Crookes
+ * @author Juan Gimenez <neojoda@gmail.com>
+ * @link http://www.montcat.org/portfolio
+ * @copyright (c) 2014 Juan Gimenez
  * @license MIT License - http://www.opensource.org/licenses/mit-license.php *
  */
 
